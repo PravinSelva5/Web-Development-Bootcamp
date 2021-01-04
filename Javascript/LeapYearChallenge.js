7af1db0acf571a07d31fa06d1d9c9083f6b4b64b
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-01-03 20:13:30 -0500

    worked on a few javascript challenges

---
+++ Javascript/LeapYearChallenge.js
@@ -0,0 +1,30 @@
+function isLeap(year) {
+
+/**************Don't change the code above****************/
+
+    //Write your code here.
+    if (year % 4 === 0){
+
+        if (year % 100 === 0) {
+            return("Not leap year.")
+        }
+
+        if (year % 100 !== 0  && year % 400 === 0){
+
+            return("Leap year.");
+        }
+        else {
+            return("Leap year.")
+        }
+
+    }
+    else {
+        return("Not leap year.");
+    }
+
+
+
+
+/**************Don't change the code below****************/
+
+}
---
+++ Javascript/LoveCalculator.js
@@ -0,0 +1,16 @@
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor((loveScore)) + 1; // 1 - 100
+
+if (loveScore > 100){
+  alert("Your love score is " + loveScore + "%" + " You guys love each other!");
+}
+if loveScore > 30 && loveScore <= 70 {
+  alert("Your love score is " + loveScore + "%");
+}
+
+if loveScore < 30 {
+  alert("Your love score is " + loveScore + "%" + " You go together like oil and water.");
+}
