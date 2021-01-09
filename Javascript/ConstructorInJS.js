5c57e49be2d48ce81d72427ed39a5599f59c920f
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-01-09 18:15:57 -0500

    added constructor js

--- Javascript/ConstructorInJS.js
+++ Javascript/ConstructorInJS.js
@@ -6,3 +6,20 @@ function HouseKeeper (yearsOfExperience, name, cleaningRepertoire) {
 }
 
 var houseKeeper1 = new HouseKeeper(10, 'Jane', ["Bedroom", "lobby", "bathroom"]);
+
+/* Constructor Function with a method calls within them */
+
+function BellBoy (name, age, hasWorkPermit, languages) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
+    this.moveSuitcase = function() {
+        alert("May I take your suitcase?");
+        pickUpSuitcase();
+        move();
+    }
+    this.cleaning = function(){
+        alert("Cleaning in progress.");
+    }
+}
