0194d0af253b0ef75a06ba20fd820ed855760061
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-01-11 13:49:49 -0500

    jQuery module

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,26 @@
-$("h1").css("color", "red");
+// This is where you add behaviour to your webpage
+
+// Event Listener in jQuery
+$("button").click(function() {
+    $("h1").css("color", "purple");
+})
+
+$(document).keydown(function(event){
+    $("h1").html(event.key);
+})
+
+
+// Animation
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown();
+})
+/* 
+
+You can add elements with jQuery with:
+- before()
+- after()
+- prepend(): adds it after the opening tag
+- append(): adds it before the closing tag
+
+*/
