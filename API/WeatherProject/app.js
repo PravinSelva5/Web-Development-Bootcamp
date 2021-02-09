ba6828f69694e0bdc4a0b993da792967f6ea91eb
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-02-08 20:15:24 -0500

    working on creating a newsletter web application

--- API/WeatherProject/app.js
+++ API/WeatherProject/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/", function(req, res){
-    
-    const query = "London";
+app.post("/", function(req, res){
+    const query = req.body.cityName;
     
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
@@ -23,13 +23,41 @@ app.get("/", function(req, res){
             const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
 
             res.write("<p>The weather is currently " + weatherDescription + "<p>");
-            res.write("<h1>The temperature in London is " + temp + " degrees Celsius.</h1>")
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius.</h1>")
             res.write("<img src="+ imageURL + ">");
             res.send();
         })
     })
 
+
 })
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+// const query = "London";
+//     const apiKey = "2df9c664dd26010d7629f1f79344b6d2";
+//     const unit = "metric";
+//     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    
+//     https.get(url, function(response){
+//         console.log(response.statusCode);
+
+//         response.on("data", function(data){
+//             const weatherData = JSON.parse(data);
+//             const temp = weatherData.main.temp;
+//             const weatherDescription = weatherData.weather[0].description;
+//             const icon = weatherData.weather[0].icon;
+//             const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+
+//             res.write("<p>The weather is currently " + weatherDescription + "<p>");
+//             res.write("<h1>The temperature in London is " + temp + " degrees Celsius.</h1>")
+//             res.write("<img src="+ imageURL + ">");
+//             res.send();
+//         })
+//     })
+
 
 
 app.listen(4000, function() {
