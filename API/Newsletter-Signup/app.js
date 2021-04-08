b68ea1f96155f59ca574648f620496fb0c6a9c3a
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-04-07 22:08:55 -0400

    worked on posting data to mailchimp servers

--- API/Newsletter-Signup/app.js
+++ API/Newsletter-Signup/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require('https');
 
 const app = express();
 
@@ -15,9 +16,42 @@ app.get("/", function(req, res){
 
 app.post('/', function(req, res){
     
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    
+    const url = 'https://us1.api.mailchimp.com/3.0/lists/5003c42f99';
+
+    const options = {
+        method: "POST",
+        auth: "pravin14:2e9c668ca9d215502983f35bbfb71498-us1"
+    }
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+    
 
     console.log(firstName, lastName, email);
 })
