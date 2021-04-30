5552e7b1860d37807130bbfdd15d7e1f3c28f6a1
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-04-29 21:36:47 -0400

    work through EJS module

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 
 const app = express();
@@ -11,18 +11,19 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    var today = new Date();
+    let today = new Date();
     
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
     
     res.render('list', 
     
@@ -30,7 +31,7 @@ app.get("/", function(req, res){
 });
 
 app.post('/', function(req, res){
-      var item = req.body.newItem;
+      let item = req.body.newItem;
       items.push(item);
 
     res.redirect("/");
