ef91a003774866211bdb55a2579753c98f06344f
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-05-01 16:30:31 -0400

    work through EJS module

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ReactSurveyElement } = require('survey-react');
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
-
+let workItems = [];
 
 const app = express();
 
@@ -27,22 +28,40 @@ app.get("/", function(req, res){
     
     res.render('list', 
     
-        { day: day, newListItems: items});
+        { listTitle: day, newListItems: items});
 });
 
 app.post('/', function(req, res){
-      let item = req.body.newItem;
-      items.push(item);
 
-    res.redirect("/");
+      let item = req.body.newItem;
+
+      if (req.body.list === "Work"){
+          workItems.push(item);
+          res.redirect("/work");
+      } else {
+        items.push(item);
+        res.redirect("/");
+      }
     
 });
 
 
 
-
+app.get("/work", function (req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 // You can use res.write() if you need to send more than one HTML tags.
 // Once you write all your res.write(), you can end it with res.send()
+
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
