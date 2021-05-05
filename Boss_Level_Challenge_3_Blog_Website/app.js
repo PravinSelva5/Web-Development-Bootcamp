29d4f544087aab004abf242570c86d0d486f38f4
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-05-05 19:20:15 -0400

    build blog project

--- Boss_Level_Challenge_3_Blog_Website/app.js
+++ Boss_Level_Challenge_3_Blog_Website/app.js
@@ -17,6 +17,11 @@ app.use(express.static("public"));
 
 
 
+app.get("/", function(req, res){
+    res.render('home.ejs');
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
